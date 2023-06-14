072bf335a8ac8e4f6c347f647e5cdaa60f46e65a
Arkadiy Voronov <voronov_ao@mail.ru>
2023-06-14 22:02:06 +0500

    change test

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -1,12 +1,14 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
+# Create ChromeOptions object and configure headless mode
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 
-@pytest.mark.usefixtures("setup")
-class TestExampleOne:
-    def test_title(self):
-        self.driver.get('https://www.delrayo.tech')
-        assert self.driver.title == "DelRayo.tech - Delrayo Tech"
-    
-    def test_title_blog(self):
-        self.driver.get('https://www.delrayo.tech/blog')
-        print(self.driver.title)
+# Initialize the ChromeDriver with the options
+driver = webdriver.Chrome(options=chrome_options)
+
+@pytest.fixture
+def browser():
